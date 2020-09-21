5ebe4f5724c2b4be3b68888707fa739737feb87c
nisozakuto <nisozakuto@gmail.com>
2020-09-20 23:38:02 -0400

    Coffee route, controller and seed data

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -1,2 +1,30 @@
 class CoffeesController < ApplicationController
+    def index
+        coffees = Coffee.all
+        render json: {coffees: coffees}
+    end
+
+    def show
+        coffee = Coffee.find(params[:id])
+        render json: {coffee: coffee}
+    end
+
+    def create 
+        coffee = Coffee.create(coffee_params)
+        render json: {coffee: coffee}
+    end
+
+    def update
+    end
+
+    def destroy
+        coffee = Coffee.find(params[:id])
+        coffee.destroy
+        head :ok
+    end
+
+    private 
+    def coffee_params
+        params.require(:coffee).permit(:coffee, :flavor, :category)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#HOT COFFEES
+Coffee.create(name:"Latte", flavor:"Espresso",category:"Hot Coffee")
+Coffee.create(name:"Caffe Americano", flavor:"Espresso",category:"Hot Coffee")
+Coffee.create(name:"Blonde Roast", flavor:"Brewed",category:"Hot Coffee")
+Coffee.create(name:"Caffè Misto", flavor:"Brewed",category:"Hot Coffee")
+Coffee.create(name:"Cappuccinos", flavor:"Cappuccinos",category:"Hot Coffee")
+Coffee.create(name:"Espresso Shots", flavor:"Espresso",category:"Hot Coffee")
+Coffee.create(name:"Flat Whites", flavor:"Ristretto",category:"Hot Coffee")
+Coffee.create(name:"Caramel Macchiato", flavor:"Espresso",category:"Hot Coffee")
+Coffee.create(name:"Mochas", flavor:"Espresso",category:"Hot Coffee")
+
+#COLD COFFEES
+Coffee.create(name:"Pumpkin Cream Cold Brew", flavor:"Pumpkin",category:"Cold Coffee")
+Coffee.create(name:"Pumpkin Cream Nitro Cold Brew", flavor:"Nitro Cold Brew",category:"Cold Coffee")
+Coffee.create(name:"Iced Caffè Americano", flavor:"Espresso",category:"Cold Coffee")
+Coffee.create(name:"Iced Coffees", flavor:"Brewed",category:"Cold Coffee")
+Coffee.create(name:"Iced Flat Whites", flavor:"Nitro",category:"Cold Coffee")
+Coffee.create(name:"Iced Caramel Macchiato", flavor:"Caramel",category:"Cold Coffee")
+
