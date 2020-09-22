e945326db20d6e11c06d465ccface535b6a2d3e8
nisozakuto <nisozakuto@gmail.com>
2020-09-22 17:40:34 -0400

    printing order ids

--- client/src/components/Users/Profile.jsx
+++ client/src/components/Users/Profile.jsx
@@ -1,11 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import { Redirect } from 'react-router-dom';
-import Auth from '../../modules/Auth'
+// import React, { useState, useEffect } from 'react';
+// import { Redirect } from 'react-router-dom';
+// import Auth from '../../modules/Auth'
 
-const Profile = () => {
-    const [data, setData] = useState(null);
-    useEffect(() => {
-        console.log('fetch the info here')
+// const Profile = () => {
+//     const [data, setData] = useState(null);
+//     useEffect(() => {
+//         fetch('/profile', {
+//             headers: {
+//                 token: Auth.getToken(),
+//                 'Authorization': `Token ${Auth.getToken()}`
+//             }
+//         })
+//             .then(res => res.json())
+//             .then(res => {
+//                 // console.log("RES", res)
+//                 setData(res)
+//             })
+//     }, []);
+
+//     const [ordersData, setOrdersData] = useState(null)
+//     useEffect(() => {
+//         fetch('/orders', {
+//             headers: {
+//                 token: Auth.getToken(),
+//                 'Authorization': `Token ${Auth.getToken()}`
+//             }
+//         })
+//             .then(res => res.json())
+//             .then(res => {
+//                 console.log("res", res)
+//                 setOrdersData(res)
+//             })
+//     })
+
+
+//     return (
+//         <div className="profile">
+//             {data ?
+//                 (
+//                     < div >
+//                         <h1>Welcome, {data.user.username}</h1>
+//                         < ul >
+//                             {
+//                                 data.orders.map(order =>
+//                                     (
+//                                         <li key={order.id}>
+//                                             <h2>User ID{order.user_id}</h2>
+//                                         </li>
+//                                     ))
+//                             }
+//                         </ul>
+//                     </div >
+//                 )
+//                 :
+//                 <p>Loading</p>
+//             }
+//             { !Auth.isUserAuthenticated() && <Redirect to="/login" />}
+
+//         </div >
+//     )
+// }
+
+// export default Profile;
+
+
+import React, { Component } from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import Auth from '../../modules/Auth'
+export default class Profile extends Component {
+    constructor() {
+        super()
+        this.state = {
+            data: null,
+            ordersData: null
+        }
+    }
+
+    componentDidMount() {
+        this.getData()
+        this.ordersData()
+    }
+
+    getData() {
         fetch('/profile', {
             headers: {
                 token: Auth.getToken(),
@@ -14,37 +90,66 @@ const Profile = () => {
         })
             .then(res => res.json())
             .then(res => {
-                setData(res)
+                this.setState({
+                    data: res
+                })
             })
-    }, []);
+    }
 
-    // fetch(`/orders`
-
-    return (
-        <div className="profile">
-            {data ?
-                (
-                    <div>
-                        <h1>{data.user.username}</h1>
-                        <ul>
-                            {data.orders.map(order =>
-                                (
-                                    <li key={order.id}>
-                                        <h2>{order.user_id}</h2>
-                                        <h2>{order.coffees_order_id}</h2>
-                                    </li>
-                                ))
-                            }
-                        </ul>
-                    </div>
-                )
-                :
-                <p>Loading</p>
+    ordersData() {
+        fetch('/orders', {
+            headers: {
+                token: Auth.getToken(),
+                'Authorization': `Token ${Auth.getToken()}`
             }
-            {!Auth.isUserAuthenticated() && <Redirect to="/login" />}
+        })
+            .then(res => res.json())
+            .then(res => {
+                this.setState({
+                    ordersData: res
+                })
+            })
+    }
 
-        </div>
-    )
+    render() {
+        return (
+            <div className="profile">
+
+                {this.state.data ?
+                    (
+                        <div>
+                            <h1>Welcome, {this.state.data.user.username}</h1>
+                            <ul>
+                                {
+                                    this.state.data.orders.map(order =>
+                                        (
+                                            <li key={order.id}>
+                                                <h2>ID{order.id}</h2>
+                                            </li>
+                                        ))
+                                }
+                            </ul>
+                            {console.log(this.state.ordersData)}
+                            <ul>
+
+                                {this.state.ordersData ?
+                                    this.state.ordersData.orders.map(order =>
+                                        (
+                                            <li key={order.id}>
+                                                <h2>Order - ID{order.id}</h2>
+                                            </li>
+                                        ))
+                                    :
+                                    <p></p>
+                                }
+                            </ul>
+                        </div>
+                    )
+                    :
+                    <p>Loading</p>
+                }
+                { !Auth.isUserAuthenticated() && <Redirect to="/login" />}
+            </div >
+        )
+    }
 }
-
-export default Profile;
