a2e929e90269b5c09508045d0310037c2af7e3a7
nisozakuto <nisozakuto@gmail.com>
2020-09-24 17:27:06 -0400

    adding order works

--- client/src/components/OrderList.jsx/AddOrder.jsx
+++ client/src/components/OrderList.jsx/AddOrder.jsx
@@ -1,11 +1,60 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import Auth from '../../modules/Auth'
 
-export default class AddOrder extends Component {
-    render() {
-        return (
-            <div>
-                New order page
-            </div>
-        )
+const AddOrder = () => {
+    const [coffee_id, setcoffeeId] = useState('')
+    const [size, setSize] = useState('')
+    const [quantity, setQuantity] = useState('')
+    const [order_id, setOrder_id] = useState('')
+
+    const handleOrderFormSubmit = (evt) => {
+        evt.preventDefault();
+        fetch('/coffee_orders', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                coffee_orders: {
+                    coffee_id, size, quantity, order_id
+                }
+            }),
+        }).then(res => res.json())
+            .then((res) => {
+                console.log(res)
+            })
     }
+
+
+    return (
+        <form onSubmit={handleOrderFormSubmit}>
+            <input
+                type="number"
+                name="coffee_id"
+                placeholder="put the coffee ID"
+                value={coffee_id}
+                onChange={(evt) => setcoffeeId(evt.target.value)} />
+            <input
+                type="number"
+                name="size"
+                placeholder="put the size"
+                value={size}
+                onChange={(evt) => setSize(evt.target.value)} />
+            <input
+                type="number"
+                name="quantity"
+                placeholder="put the quantity"
+                value={quantity}
+                onChange={(evt) => setQuantity(evt.target.value)} />
+            <input
+                type="number"
+                name="order_id"
+                placeholder="put the order_id"
+                value={order_id}
+                onChange={(evt) => setOrder_id(evt.target.value)} />
+            <input type="submit" value="Add to the Order" />
+        </form>
+    )
 }
+
+export default AddOrder;
